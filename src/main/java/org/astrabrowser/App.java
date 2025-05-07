2170b92559c85e38720d24aae153c25b4f689d32
Tharun R  S <rstharun11@gmail.com>
2025-05-07 19:03:22 +0530

    simple browser

--- src/main/java/org/astrabrowser/App.java
+++ src/main/java/org/astrabrowser/App.java
@@ -1,13 +1,23 @@
 package org.astrabrowser;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        WebView webView = new WebView();
+        webView.getEngine().load("https://google.com");
+
+        Scene scene = new Scene(webView, 1200, 800);
+        stage.setTitle("Astra Browser");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
